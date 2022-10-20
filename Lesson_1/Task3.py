ca747715c702dbe662d786870990d0199bf7062b
DenKN22 <116087731+DenKN22@users.noreply.github.com>
2022-10-20 15:22:17 +0300

    Update Task3.py

--- Lesson_1/Task3.py
+++ Lesson_1/Task3.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 seconds = int(input("Write count of seconds"))
-date = datetime.fromtimestamp(seconds)
-print(date)
-
+day = seconds//86400
+hour = (seconds%86400)//3600
+minute = (seconds%3600)//50
+sec = (seconds%60)%60
+print(day,"day",hour,"hour",minute,"minutes",sec,"seconds")
